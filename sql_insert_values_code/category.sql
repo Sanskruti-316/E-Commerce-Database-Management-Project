71e624e40c45ddcbb424e026d385d9e4fa319826
Sanskruti Agasgekar <01fe24bcs169@kletech.ac.in>
2026-01-05 16:05:03 +0530

    Add new categories to e_commerce database

--- sql_insert_values_code/category.sql
+++ sql_insert_values_code/category.sql
@@ -2,3 +2,7 @@ insert into e_commerce.category values (1,'Mobiles & Computer','all the brands a
 insert into e_commerce.category values (2,'TV & Aplliances & Electronics','all the brands are there like tv smart, tv oled, mixer and many more'); 
 insert into e_commerce.category values (3,'Men`s Fashion','all the brands are there like t-Shirts, jeans, shirts,etc'); 
 insert into e_commerce.category values (4,'Women`s Fashion','all the brands are there like shorts,one pic, kurti, t-shirt,jeans,etc');
+insert into e_commerce.category values (5,'Books & Stationery','all types of books, notebooks, pens, etc');
+insert into e_commerce.category values (6,'Sports & Fitness','sports equipment, gym accessories, yoga mats');
+insert into e_commerce.category values (7,'Home & Kitchen','furniture, kitchen appliances, decor items');
+insert into e_commerce. category values (8,'Beauty & Personal Care','cosmetics, skincare, haircare products');
