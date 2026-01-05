d5446db11723ada2d974dc650a83d09e51ea8a71
Sanskruti Agasgekar <01fe24bcs169@kletech.ac.in>
2026-01-05 16:09:00 +0530

    Insert additional address records into database
    Added multiple new address entries to the e_commerce.address table.

--- sql_insert_values_code/address.sql
+++ sql_insert_values_code/address.sql
@@ -1,3 +1,8 @@
 insert into e_commerce.address values (1,'108','khodiayr chs ltd','devipada borivali east','maharsahtra','mumbai','400066',1);
 insert into e_commerce.address values (2,'214/B','vitthal chs','rattan nagar kandivali east','maharsahtra','mumbai','400801',2);
 insert into e_commerce.address values (3,'52','oberoi sky city','thakur complex malad east','maharsahtra','mumbai','400526',3);
+insert into e_commerce.address values (4,'305','shanti niwas','link road goregaon west','maharashtra','mumbai','400062',4);
+insert into e_commerce.address values (5,'12/A','sunrise apartments','sv road andheri west','maharashtra','mumbai','400058',5);
+insert into e_commerce.address values (6,'78','green valley','powai','maharashtra','mumbai','400076',6);
+insert into e_commerce.address values (7,'45/B','crystal tower','lokhandwala complex','maharashtra','mumbai','400053',7);
+insert into e_commerce.address values (8,'89','royal palace','bandra west','maharashtra','mumbai','400050',8);
